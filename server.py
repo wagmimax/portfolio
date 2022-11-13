a335e280281c3a5c7bc97861cfd9895f304d795d
Maxim Kazak <maximkazak@protonmail.com>
2022-11-13 11:22:49 -0500

    my website

---
+++ server.py
@@ -0,0 +1,30 @@
+#https://flask.palletsprojects.com/en/2.2.x/
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+@app.route("/<string:page_name>")
+def about_page(page_name):
+    return render_template(f'{page_name}.html')
+
+def write_to_csv(data):
+    with open('database.csv', newline='', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+@app.route("/submit_form", methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/response')
+    else:
+        return 'Something went wrong.'
